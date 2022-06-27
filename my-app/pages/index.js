4ceecba6830d7a3bfb8db38ef9cd50984d4a0e98
firebat75 <jacklowry1337@gmail.com>
2022-06-27 08:50:37 -0400

    added markup for home page to index.js

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -271,6 +271,33 @@ export default function Home() {
     }
   }
 
+  return (
+    <div>
+      <Head>
+        <title>Crypto Devs</title>
+        <meta name="description" content="Whitelist-Dapp" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to Crypto Devs!</h1>
+          <div className={styles.description}>
+            Its an NFT collection for developers in Crypto.
+          </div>
+          <div className={styles.description}>
+            {tokenIdsMinted}/20 have been minted
+          </div>
+          {renderButton()}
+        </div>
+        <div>
+          <img className={styles.image} src="./cryptodevs/0.svg" />
+        </div>
+      </div>
 
+      <footer className={styles.footer}>
+        Made with &#10084; by Crypto Devs
+      </footer>
+    </div>
+  );
 
 }
